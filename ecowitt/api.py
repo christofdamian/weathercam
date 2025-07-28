2642f437cf0ac8425033af0caa90399e7e238250
Christof Damian <christof@damian.net>
2025-07-28 18:02:26 +0200

    some work on the template

--- ecowitt/api.py
+++ ecowitt/api.py
@@ -3,15 +3,31 @@ import requests
 class Ecowitt:
     data = None
 
-    def __init__(self, application_key=None, api_key=None, mac=None, temp_unitid=2, call_back="all"):
+    def __init__(self, application_key=None, api_key=None, mac=None, temp_unitid=2, wind_speed_unitid=7, call_back="all"):
         response = requests.get(f"https://api.ecowitt.net/api/v3/device/real_time", params = {
             "application_key": application_key,
             "api_key": api_key,
             "mac": mac,
             "temp_unitid": temp_unitid,
+            "wind_speed_unitid": wind_speed_unitid,
             "call_back": call_back,
         })
         self.data = response.json()["data"]
 
     def outdoor_temperature_value(self):
         return self.data["outdoor"]["temperature"]["value"]
+
+    def outdoor_temperature_unit(self):
+        return self.data["outdoor"]["temperature"]["unit"]
+
+    def outdoor_humidity_value(self):
+        return self.data["outdoor"]["humidity"]["value"]
+
+    def outdoor_humidity_unit(self):
+        return self.data["outdoor"]["humidity"]["unit"]
+
+    def wind_speed_value(self):
+        return self.data["wind"]["wind_speed"]["value"]
+
+    def wind_speed_unit(self):
+        return self.data["wind"]["wind_speed"]["unit"]
--- weathercam.py
+++ weathercam.py
@@ -11,13 +11,15 @@ load_dotenv()
 ecowitt_application_key = os.environ.get("APPLICATION_KEY")
 ecowitt_api_key = os.environ.get("API_KEY")
 ecowitt_mac = os.environ.get("MAC")
-ecowitt_temp_unitid = os.environ.get("TEMP_UNITID")
+ecowitt_temp_unitid = int(os.environ.get("TEMP_UNITID"))
+ecowitt_wind_speed_unitid = int(os.environ.get("WIND_SPEED_UNITID"))
 
 ecowitt = ecowitt.api.Ecowitt(
-    application_key = ecowitt_application_key,
-    api_key = ecowitt_api_key,
-    mac = ecowitt_mac,
-    temp_unitid = ecowitt_temp_unitid
+    application_key=ecowitt_application_key,
+    api_key=ecowitt_api_key,
+    mac=ecowitt_mac,
+    temp_unitid=ecowitt_temp_unitid,
+    wind_speed_unitid=ecowitt_wind_speed_unitid,
     )
 
 
@@ -29,4 +31,11 @@ jinja2  = Environment(
 for filename in ["weathercam.html"]:
     print(filename)
     template = jinja2.get_template(filename)
-    template.stream(temperature=ecowitt.outdoor_temperature_value()).dump("output/" + filename)
+    template.stream(
+        temperature_value=ecowitt.outdoor_temperature_value(),
+        temperature_unit=ecowitt.outdoor_temperature_unit(),
+        humidity_value=ecowitt.outdoor_humidity_value(),
+        humidity_unit=ecowitt.outdoor_humidity_unit(),
+        wind_speed_value=ecowitt.wind_speed_value(),
+        wind_speed_unit=ecowitt.wind_speed_unit(),
+    ).dump("output/" + filename)
