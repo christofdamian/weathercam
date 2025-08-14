35808d1f99f6ceb47243b41adcaccd44c84d3a50
Christof Damian <cdamian@stubhubinternational.com>
2025-08-14 11:19:54 +0200

    separate css into file

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ echo "* getting snapshot"
 echo "* getting weather"
 ./weathercam.py
 
+echo "* copy static files"
+cp -avu static output/
+
 echo "* uploading files"
 aws s3 cp --recursive output/. s3://webcam.calpenedes.com/
 
