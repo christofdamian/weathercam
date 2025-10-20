f9bbe2522d9071f1d407b84d6913c562b789b43f
Christof Damian <christof@damian.net>
2025-10-20 09:31:08 +0200

    snap error handling

--- snap.py
+++ snap.py
@@ -13,13 +13,19 @@ HUB_PASSWORD = os.environ.get("REOLINK_HUB_PASSWORD")
 HUB_CAMERA_ID = int(os.environ.get("REOLINK_HUB_CAMERA_ID"))
 
 async def snap():
-    host = Host(HUB_IP, HUB_USER, HUB_PASSWORD)
-    await host.get_host_data()
+    host = Host(HUB_IP, HUB_USER, HUB_PASSWORD, timeout=10)
+    try:
+        await asyncio.wait_for(host.get_host_data(), timeout=15)
 
-    with open("output/snap.jpg", "wb") as image:
-        image.write(await host.get_snapshot(HUB_CAMERA_ID))
-
-    await host.logout()
+        with open("output/snap.jpg", "wb") as image:
+            snapshot = await asyncio.wait_for(host.get_snapshot(HUB_CAMERA_ID), timeout=10)
+            image.write(snapshot)
+    finally:
+        try:
+            await asyncio.wait_for(host.logout(), timeout=5)
+            await asyncio.wait_for(host.unsubscribe(), timeout=5)
+        except asyncio.TimeoutError:
+            pass
 
 if __name__ == "__main__":
     asyncio.run(snap())
