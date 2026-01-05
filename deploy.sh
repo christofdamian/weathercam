841fd1475c2481e4c2d3d1c12c0d11493758ff44
Christof Damian <christof@damian.net>
2026-01-05 09:27:09 +0100

    improved upload

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ echo "* copy static files"
 cp -avu static output/
 
 echo "* uploading files"
-aws s3 cp --recursive output/. s3://webcam.calpenedes.com/
+aws s3 sync output/. s3://webcam.calpenedes.com/ --cache-control "max-age=600"
 
 exit 0
